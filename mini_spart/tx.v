120c958fc94edb4a35060eea0c814c0a66a16111
John L. Peterson <jackgopack4@gmail.com>
2015-01-30 12:56:14 -0600

    TX supposedly working?

--- mini_spart/tx.v
+++ mini_spart/tx.v
@@ -17,7 +17,7 @@ module tx(
 // Assert TxD_start for (at least) one clock cycle to start transmission of TxD_data
 // TxD_data is latched so that it doesn't have to stay valid while it is being sent
 
-reg TxD_state;
+reg [1:0] TxD_state;
 wire TBR = (TxD_state==0);
 reg [3:0] EnableCnt;
 reg [3:0] StateCnt;
@@ -28,7 +28,7 @@ always @(posedge clk, negedge rst_n) begin
 		Sample <= 0;
 		EnableCnt <= 0;
 		StateCnt <= 0;
-		TxD_state <= 1'b0;
+		TxD_state <= 2'b00;
 	end
 	Sample <= 0;
 	if(Enable) begin
@@ -39,46 +39,41 @@ always @(posedge clk, negedge rst_n) begin
 		else begin
 			EnableCnt <= EnableCnt + 1;
 		end
-		$display("EnableCnt = %b", EnableCnt);
+		//$display("EnableCnt = %b", EnableCnt);
 	end
+	//$display("Sample = %b", Sample);
 	if(Sample) begin
 		if(StateCnt == 4'b1010) begin
 			StateCnt <= 0;
 		end
 		else begin
-			if(TxD_state == 1'b1) begin
+			if(TxD_state > 2'b00) begin
 				StateCnt <= StateCnt + 1;
 				TxD_shift <= (TxD_shift >> 1);
 			end
 		end
 		$display("StateCnt = %b", StateCnt);
+		$display("TxD_state = %b", TxD_state);
 	end
-	if(TBR & TxD_start)
+	if(TBR && TxD_start) begin
 		TxD_shift <= TxD_data;
 		$display("data latched");
+	end
 end
 
 reg [7:0] TxD_shift = 0;
-always @(posedge clk, negedge rst_n)
+always @(posedge clk)
 begin
-	if(!rst_n) begin
-		TxD_state <= 1'b0;
-	end
-	if(TBR & TxD_start)
-		TxD_shift <= TxD_data;
-	else
-	if(TxD_state == 1'b1; & Sample)
-		TxD_shift <= (TxD_shift >> 1);
-
+	//$display("TxD_state = %b", TxD_state);
 	case(TxD_state)
-		1'b0: if(TxD_start) TxD_state <= 1'b1; // idle
-		1'b1: if(Sample) begin // transmit
-			if(StateCnt < 10) TxD_state <= 1'b1;
-			else TxD_state <= 1'b0
-		end
-		default: TxD_state <= 1'b0;
+		2'b00: 	if(TxD_start) TxD_state <= 2'b10; // idle
+		2'b01: 	if(StateCnt >= 4'b1010) TxD_state <= 2'b00;
+		2'b10: 	if(Sample) begin // transmit
+					TxD_state <= 2'b01;
+			   	end
+		default: TxD_state <= 2'b00;
 	endcase
 end
 
-assign TxD = (TxD_state == 1'b1 && !(StateCnt >= 4'b1001) && (StateCnt == 4'b0001 | TxD_shift[0]));  // put together the start, data and stop bits
+assign TxD = (TxD_state > 2'b00 && (StateCnt == 4'b0001 | TxD_shift[0]));  // put together the start, data and stop bits
 endmodule
--- mini_spart/tx_tb.v
+++ mini_spart/tx_tb.v
@@ -9,13 +9,21 @@ module tx_tb();
 
 	tx iDUT(clk, rst_n, TxD_start, Enable, TxD_data, TxD, TBR);
 	initial begin
+		$dumpfile("test_tx.vcd");
+		$dumpvars(0, iDUT);
 		clk = 0;
 		rst_n = 0;
 		TxD_start = 0;
 		Enable = 0;
-		TxD_data = 8'b01010101;
+		TxD_data = 8'b10101010;
 		#12;
 		rst_n = 1;
+		#3;
+		TxD_start = 1'b1;
+		#20;
+		TxD_start = 1'b0;
+		#50000;
+		$stop;
 
 	end
 	always begin
@@ -28,4 +36,11 @@ module tx_tb();
 		#5;
 		Enable = 0;
 	end
+	always begin
+		#475;
+		$display("TxD = %b", TxD);
+		$display("TBR = %b", TBR);
+		#5;
+	end
+
 endmodule
