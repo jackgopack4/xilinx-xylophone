2b6833545a17430e1ce17d7787d30e433947f1e5
tzodrow <tszodr@gmail.com>
2015-01-30 12:07:12 -0600

    Created baud_rate_gen

--- mini_spart/baud_rate_gen.v
+++ mini_spart/baud_rate_gen.v
@@ -18,16 +18,25 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-module baud_rate_gen(tx_ben, rx_ben, clk, rst, enable, data, write, sel_low, sel_high);
+module baud_rate_gen(
+	//output tx_ben, 
+	//output rx_ben, 
+	input clk, 
+	input rst, 
+	output en, 
+	input [7:0] data, 
+	input sel_low, 
+	input sel_high
+	);
 
-input clk, rst, enable;
-input [7:0] data;  //for divisor
+reg [15:0] divisor_buffer;
+reg state, nxt_state;
+reg [11:0] counter;
 
-output tx_bd_en, rx_bd_en;  //baud enable for transmitting and receiving 
+wire load_counter, en_counter;
 
-reg [15:0] divisor_buffer, rx_bd_cnt, tx_bd_cnt; //counters for tx and rx
-
-reg [32:0] divisor;
+localparam SEND_SIGNAL = 0;
+localparam WAIT = 1;
 
 always @ (posedge clk, posedge rst)
 	if(rst)
@@ -37,7 +46,50 @@ always @ (posedge clk, posedge rst)
 	else if(sel_high)
 		divisor_buffer <= {data, divisor_buffer[7:0]};
 		
-		// TODO HARDWIRE VALUES
+always @ (posedge clk, posedge rst)
+	if(rst)
+		state <= SEND_SIGNAL;
+	else
+		state <= nxt_state;
+		
+always @ (posedge clk, posedge rst)
+	if(rst)
+		counter <= 12'h000;
+	else if(load_counter)
+		counter <= counter_start;
+	else if(en_counter)
+		counter <= counter - 1;
+		
+always @ (*) begin
+	counter_start = 12'h000;
+	case(divisor_buffer[15:12])
+		4'h1 : counter_start = 12'h515;
+		4'h2 : counter_start = 12'h28A;
+		4'h4 : counter_start = 12'h145;
+		4'h9 : counter_start = 12'h0A3;
+	endcase
+end
 
-assign 
+
+always @ (clk, rst, state, counter) begin
+	en = 0;
+	load_counter = 0;
+	en_counter = 0;
+	nxt_state = SEND_SIGNAL;
+	case(state)
+		SEND_SIGNAL : begin
+			en = 1;
+			load_counter = 1;
+			nxt_state = WAIT;
+		end
+		WAIT : begin
+			if(~(|counter))
+				nxt_state = SEND_SIGNAL;
+			else begin
+				en_counter = 1;
+				nxt_state = WAIT;
+			end
+		end
+	endcase
+end
 endmodule
