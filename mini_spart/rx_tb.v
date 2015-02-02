f5eb5fa781c857fa1956b96a9db9eef070774e06
John L. Peterson <jackgopack4@gmail.com>
2015-02-02 12:50:36 -0600

    Change Rx2 to Rx

--- mini_spart/rx_tb.v
+++ mini_spart/rx_tb.v
@@ -3,7 +3,7 @@ module rx_tb();
 	wire [7:0] RxD_data;
 	wire RDA;
 
-	rx2 iDUT(clk, rst, RxD, Baud, RxD_data, RDA);
+	rx iDUT(clk, rst, RxD, Baud, RxD_data, RDA);
 	initial begin
 		$dumpfile("test_rx.vcd");
 		$dumpvars(0, iDUT);
@@ -15,6 +15,7 @@ module rx_tb();
 		rst = 0;
 		#3;
 		#20;
+		#1000;
 		RxD = 0; // Start bit
 		#480;
 		RxD = 1; // bit 0
@@ -38,9 +39,33 @@ module rx_tb();
 		#480;
 		#480; // Stop 2
 		$display("second stop bit sent");
-		#50000;
+		// send second data
+		#1000;
+		RxD = 0; // Start bit
+		#480;
+		RxD = 0; // bit 0
+		#480;
+		RxD = 1; // bit 1
+		#480;
+		RxD = 0; // bit 2
+		#480;
+		RxD = 1; // bit 3
+		#480;
+		RxD = 0; // bit 4
+		#480;
+		RxD = 1; // bit 5
+		#480;
+		RxD = 0; // bit 6
+		#480;
+		RxD = 1; // bit 7
+		#480;
+		$display("last bit sent");
+		RxD = 1; // Stop 1
+		#480;
+		#480; // Stop 2
+		$display("second stop bit sent");
 		$stop;
-
+		$finish;
 	end
 	always begin
 		#5;
@@ -56,6 +81,7 @@ module rx_tb();
 		#475;
 		$display("RxD_data = %b", RxD_data);
 		$display("RDA = %b", RDA);
+		$display("RxD = %b", RxD);
 		#5;
 	end
 
