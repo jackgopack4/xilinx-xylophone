98c602778dd8cfa956e02a2a3cd6380b1d8977d1
tzodrow <tszodr@gmail.com>
2015-02-03 14:14:10 -0600

    Manji Comments

--- mini_spart/bus_interface.v
+++ mini_spart/bus_interface.v
@@ -1,3 +1,22 @@
+//////////////////////////////////////////////////////////////////////////////////////////
+// Company: UW Madison 
+// Engineers: Tim Zodrow, Manjot S Pal, Jack
+// 
+// Create Date: 2/2/2015  
+// Design Name: mini-spart
+// Module Name:    bus_interface 
+// Project Name: miniproject1
+// Target Devices: FPGA
+// Description: The Bus Interface constitutes a 3 state buffer which connects the SPART to 
+//the DATABUS. It contains combinational logic for the selecting the lower or higher bits. 
+//It utilizes IOCS and IOR/W signals to make sure that 3 state drivers don't in conflict with 
+//other drivers on DATABUS.
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+
+/////////////////////
+// Inputs, outputs //
+////////////////////
 module bus_interface(
 	input iocs,
 	input iorw,
@@ -15,6 +34,7 @@ module bus_interface(
 	output reg databus_sel
 	);
 	
+//defaulting/initializing the signals 
 	always @ (*) begin
 		data_out = 8'h00;
 		wrt_db_low = 0;
@@ -23,6 +43,8 @@ module bus_interface(
 		databus_sel = 0;
 		databus_out = 8'h00;
 		rd_rx = 0;
+
+//getting into different cases only when chip select is high
 		if(iocs) begin
 			case(ioaddr)
 				2'b00 : begin
@@ -39,19 +61,24 @@ module bus_interface(
 					end
 				end
 				2'b01 : begin
+					//Status register
 					if(iorw) begin
+						//setting the databus select high
 						databus_sel = 1;
 						databus_out = {6'b000000, rda, tbr};
 					end
 				end
 				2'b10 : begin
+					//low division buffer
 					data_out = databus_in;
-					wrt_db_low = 1;
+					wrt_db_low = 1;		//setting the low bits signal high to indicate to fill out the lower bits
 				end
 				2'b11 : begin
+					//high division buffer
 					data_out = databus_in;
-					wrt_db_high = 1;
+					wrt_db_high = 1;	//setting the high bits signal high to indicate to fill out the higher bits
 				end
+			//ending the case statement
 			endcase
 		end
 	end
