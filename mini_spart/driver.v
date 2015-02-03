aebaf57a92a3c47e13e179c4b5756bf97fd590ab
Tim Zodrow <tszodr@gmail.com>
2015-02-02 23:45:35 -0600

    Tim's changes to comments and fixes for spart and driver.

--- mini_spart/driver.v
+++ mini_spart/driver.v
@@ -1,23 +1,18 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
+//////////////////////////////////////////////////////////////////////////////////////////
+// Company: UW Madison 
+// Engineers: Tim Zodrow, Manjot S Pal, Jack
 // 
-// Create Date:    
-// Design Name: 
-// Module Name:    driver 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
+// Create Date: 2/2/2015  
+// Design Name: mini-spart
+// Module Name: driver 
+// Project Name: miniproject1
+// Target Devices: FPGA
+// Description: The driver starts initializes and drives the sparts transactions and receives
+// data from the databus. It sets the baud rate from the br_cfg and sends out signals to 
+// capture the data from the spart and send data back.
+
+//////////////////////////////////////////////////////////////////////////////////////////////
 module driver(
     input clk,
     input rst,
@@ -29,49 +24,38 @@ module driver(
     output reg [1:0] ioaddr,
     inout [7:0] databus
     );
-	 
-	 reg baud_rate;
-	 reg [2:0] state, nxt_state;
-	 reg sel,wrt_rx_data;
-	 
-	// localparam IDLE = 3'b000;
-	 localparam INIT_LOW_DB = 2'b00;
-	 localparam INIT_HIGH_DB = 2'b01;
-	 localparam RECEIVE = 2'b11;
-   // localparam TRANSMIT = 3'b100;
-	 localparam RECEIVE_WAIT = 2'b10;
-	 //localparam TRANSMIT_WAIT = 3'b110;
-	 
-	 
-	 reg [7:0] a, data_out, rx_data;
-	 wire [7:0] b;
-	// Select high when writing to databus
-	// reading from databus
+
+    //////////////////////////////
+    // States for State Machine //
+    //////////////////////////////
+    localparam INIT_LOW_DB = 2'b00;
+    localparam INIT_HIGH_DB = 2'b01;
+    localparam RECEIVE = 2'b11;
+    localparam RECEIVE_WAIT = 2'b10;
+
+    ////////////////////////////////////////
+    // Registers used for control signals //
+    ////////////////////////////////////////
+    reg sel,wrt_rx_data;
+
+    // State Registers
+    reg [2:0] state, nxt_state;
+    // Data Registers
+    reg [7:0] data_out, rx_data;
+
+    // Tri-state buffer used to receive and send data via the databuse
+    // Sel high = output, Sel low = input
 	assign databus = sel ? data_out : 8'bz;
-	assign b = databus;
 	
-/*
-	always @ (posedge clk, posedge rst) begin
-	
-		if(rst) begin
-			a <= 8'h00;
-			b <= 8'h00;
-			end
-		else begin
-			a <= data_out;
-			b <= databus;
-			end		
-		
-		end
-*/
-		
+    // RX Received Data Flop
 	always  @ (posedge clk, posedge rst) begin
 		if(rst)
 			rx_data <= 8'h00;
 		else if (wrt_rx_data)
-			rx_data <= b;
+			rx_data <= databus;
 	end
 	
+    // State Flop
 	always @ (posedge clk, posedge rst) begin
 		if(rst)
 			state <= 2'b00;
@@ -79,9 +63,11 @@ module driver(
 			state <= nxt_state;
 	end
 
-	 
-	 always@(*) begin
-		baud_rate = 16'h12c0; 				//default baud rate to 4800
+	///////////////////
+    // State Machine //
+    ///////////////////
+    always@(*) begin
+        // Initializations
 		ioaddr = 2'b00;
 		sel = 0;
 		iocs = 1;
@@ -90,25 +76,8 @@ module driver(
 		data_out = 8'h00;
 		wrt_rx_data = 0;
 		
-		/*
-		case(br_cfg) 
-	
-		2'b00: 
-				baud_rate = 16'h12c0;		//baud rate to 4800
-				
-		2'b01:
-				baud_rate = 16'h2580;		//baud rate to 9600
-				
-		2'b10: 
-				baud_rate = 16'h4b00;		//baud rate to 19200
-				
-		2'b11:
-				baud_rate = 16'h9600;		//baud rate to 38400
-
-		endcase
-		*/
-		
 		case(state)
+            // Write the lower byte to Baud Gen
 			INIT_LOW_DB: begin 
 					ioaddr = 2'b10;
 					sel = 1;
@@ -124,7 +93,7 @@ module driver(
 								data_out = 8'h00;		//baud rate to 38400
 					endcase	
 			end
-			
+			// Write the higher byte to Baud Gen
 			INIT_HIGH_DB: begin
 					ioaddr = 2'b11;
 					sel = 1;
@@ -140,12 +109,11 @@ module driver(
 								data_out = 8'h96;		//baud rate to 38400
 					endcase	
 			end
-			
+			// Wait for receive data to be read
 			RECEIVE_WAIT: begin
 					if(~rda) begin
 						nxt_state = RECEIVE_WAIT;
-						iocs = 0;					// TODO: Maybe better to do this? -- TEST
-						// ioaddr = 2'b00;
+						iocs = 0;
 					end
 					else begin
 						nxt_state = RECEIVE;
@@ -153,7 +121,7 @@ module driver(
 						ioaddr = 2'b00;
 					end
 			end
-			
+			// Send receive data to TX when TX is ready for data
 			RECEIVE: begin
 				if(tbr) begin
 					nxt_state = RECEIVE_WAIT;					
--- mini_spart/spart.v
+++ mini_spart/spart.v
@@ -1,23 +1,20 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
+//////////////////////////////////////////////////////////////////////////////////////////
+// Company: UW Madison 
+// Engineers: Tim Zodrow, Manjot S Pal, Jack
 // 
-// Create Date:   
-// Design Name: 
-// Module Name:    spart 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
+// Create Date: 2/2/2015  
+// Design Name: mini-spart
+// Module Name: spart
+// Project Name: miniproject1
+// Target Devices: FPGA
+// Description: The spart is the main controller that brings all the modules together and 
+// performs the intreactions between the serial input (rx) and output (tx) as well as the
+// databus and signals from the driver. It connects the bus interface, tx, rx, and baud generator
+// together
+
+//////////////////////////////////////////////////////////////////////////////////////////////
 module spart(
     input clk,
     input rst,
@@ -29,72 +26,63 @@ module spart(
     inout [7:0] databus,
     output txd,
     input rxd
-	// output tx_rx_en_out		// Used for testing
     );
 	 
-	reg [7:0] a,b;
+    ///////////////////////////////////
+    // Interconnects between modules //
+    ///////////////////////////////////
 	wire [7:0] databus_out, bus_interface_out, rx_data_out;
 	wire sel, wrt_db_low, wrt_db_high, wrt_tx, tx_rx_en, rd_rx;
 
-	// Select high when writing to databus
-	// reading from databus
+	// Tri-state buffer used to receive and send data via the databuse
+    // Sel high = output, Sel low = input
 	assign databus = sel ? databus_out : 8'bzz;
-	
-	//always @ (posedge clk, posedge rst)
-	//	if(rst) begin
-	//		a <= 8'h00;
-	//		b <= 8'h00;
-	//	end
-	//	else begin
-	//		a <= databus_out;
-	//		b <= databus;
-	//	end		
 
-	// Testing //
-	/////////////
-	assign tx_rx_en_out = tx_rx_en;
-
+    // Bus Interface Module
     bus_interface bus0( .iocs(iocs),
                         .iorw(iorw),
                         .ioaddr(ioaddr),
                         .rda(rda),
                         .tbr(tbr),
                         .databus_in(databus),
-								.databus_out(databus_out),
-                        .data_in(rx_data_out), // TODO: From RX
+						.databus_out(databus_out),
+                        .data_in(rx_data_out),
                         .data_out(bus_interface_out),
                         .wrt_db_low(wrt_db_low),
                         .wrt_db_high(wrt_db_high),
                         .wrt_tx(wrt_tx),
-								.rd_rx(rd_rx),
-								.databus_sel(sel)
+						.rd_rx(rd_rx),
+						.databus_sel(sel)
                         );
-								
+
+	// Baud Rate Generator Module						
 	baud_rate_gen baud0(	.clk(clk), 
-								.rst(rst), 
-								.en(tx_rx_en), 
-								.data(bus_interface_out), 
-								.sel_low(wrt_db_low), 
-								.sel_high(wrt_db_high)
-								);
-								
+							.rst(rst), 
+							.en(tx_rx_en), 
+							.data(bus_interface_out), 
+							.sel_low(wrt_db_low), 
+							.sel_high(wrt_db_high)
+							);
+
+	// TX Module (Sends data out)				
 	tx tx0(	.clk(clk),
-				.rst(rst),
-				.data(bus_interface_out),
-				.en(tx_rx_en),
-				.en_tx(wrt_tx),
-				.tbr(tbr),
-				.TxD(txd)
-				);
+			.rst(rst),
+			.data(bus_interface_out),
+			.en(tx_rx_en),
+			.en_tx(wrt_tx),
+			.tbr(tbr),
+			.TxD(txd)
+			);
 	
+    // RX Module (Recieves data in)
 	rx rx0(	.clk(clk),
-				.rst(rst),
-				.RxD(rxd),
-				.Baud(tx_rx_en),
-				.RxD_data(rx_data_out),
-				.RDA(rda),
-				.rd_rx(rd_rx)
-		);
+			.rst(rst),
+			.RxD(rxd),
+			.Baud(tx_rx_en),
+			.RxD_data(rx_data_out),
+			.RDA(rda),
+			.rd_rx(rd_rx)
+	        );
 
 
 endmodule
