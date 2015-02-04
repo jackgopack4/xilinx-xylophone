6d0729d7d07b57c13d6d0cae3cd0e5c7827a391d
Tim Zodrow <tszodr@gmail.com>
2015-02-04 17:17:35 -0600

    Updated ROM

--- VGA/rom.v
+++ VGA/rom.v
@@ -3,14 +3,16 @@
 module rom(
 	input clk,
 	input rst,
-	input [12:0] addr, 
+	input [23:0] addr, 
 	output [23:0] rdata);
 	
-	parameter ADDR_WIDTH = 13;
+
+	parameter DISPLAY_HEIGHT = 640;
+	parameter DISPLAY_WIDTH = 480;
 	parameter DATA_WIDTH = 24;
 	parameter MEM_FILE = "tiny_image.coe";
 
-	reg [DATA_WIDTH-1:0] ROM [0:2**ADDR_WIDTH-1];
+	reg [DATA_WIDTH-1:0] ROM [0:DISPLAY_HEIGHT*DISPLAY_WIDTH-1];
 	initial $readmemh(MEM_FILE, ROM);
 	always @ (posedge clk, posedge rst)
 		if(rst)
