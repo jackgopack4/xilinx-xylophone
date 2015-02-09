30e14d5505e6902189c11705ff16897d66906bc2
tzodrow <tszodr@gmail.com>
2015-02-08 20:46:38 -0600

    Updated testbench

--- VGA/Testbench/read_rom_tb.v
+++ VGA/Testbench/read_rom_tb.v
@@ -1,12 +1,18 @@
+`timescale 1ns / 1ps
 module read_rom_tb();
 
-module rom(
+wire write_en_mon;
+wire [23:0] rom_data, display_addr, data_out_mon;
+
+reg stm_empty, stm_full, stm_clk, stm_rst;
+
+rom rom0(
 	stm_clk,
 	stm_rst,
 	display_addr, 
 	rom_data);
 
-module display_pane(
+display_pane display0(
     stm_clk,
     stm_rst,
     rom_data,
@@ -17,11 +23,6 @@ module display_pane(
     data_out_mon
     );
 
-wire write_en_mon;
-wire [23:0] rom_data, display_addr, data_out_mon;
-
-reg stm_empty, stm_full, stm_clk, stm_rst;
-
 always
 	#5 stm_clk <= ~stm_clk;
 
@@ -31,11 +32,13 @@ initial begin
 	stm_full = 0;
 	stm_empty = 0;
 
-	@(posedge clk);
+	@(posedge stm_clk);
 	stm_rst = 0;
 
 	repeat(512) @ (posedge stm_clk);
 
 	$stop();
+
+end
 
 endmodule
